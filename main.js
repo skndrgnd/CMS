78bd6cfa339b7f64744b3343ef6b274925cc4662
Sikandar Gond <147004407+skndrgnd@users.noreply.github.com>
2024-07-09 10:04:30 +0530

    CMS
    Collage Management System

---
+++ main.js
@@ -0,0 +1,25 @@
+const counts = document.querySelectorAll('.count')
+const speed = 97
+counts.forEach((counter) => {
+    function upData() {
+        const target = Number(counter.getAttribute('data-target'))
+        const count = Number(counter.innerText)
+        const inc = target / speed
+        if (count < target) {
+            counter.innerText = Math.floor(inc + count)
+            setTimeout(upData, 1)
+        } else {
+            counter.innerText = target
+        }
+    }
+    upData()
+})
+
+
+// modal
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+    myInput.focus()
+})
